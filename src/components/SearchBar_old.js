9e576ed26cbef121830076f6316a0f0c0807db67
Carr <srcarr@amazon.com>
2018-12-31 07:03:21 -0500

    Searchbar Controlled Elements

--- src/components/SearchBar_old.js
+++ src/components/SearchBar_old.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: '' }
+    onInputChange(event){
+        event.target.value
+    }
 
     render () {
         return (
@@ -9,7 +11,7 @@ class SearchBar extends React.Component {
             <form className="ui form">
                 <div className="field">
                     <label>Image Search</label>
-                    <input type="text" value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })}/>
+                    <input type="text" onChange={this.onInputChange}/>
                 </div>
             </form>
         </div>
@@ -18,10 +20,11 @@ class SearchBar extends React.Component {
 }
 // We do not put parenthesis '()' on event handlers like onChange... This will call this function when it is rendered. By leaving the '()' off we are telling it that
 // this function is going to be called later and that the value of the text input will be what goes into the '()'.
-// 
-// Controlled vs Uncontrolled Elements
-// React should be driving all the changes... This may seem redundant... But very clearly React is controlling the dom element.
-// 
+
+// alt syntax
+//  
+// onChange{(event) => console.log(event.target.value)} <---- Use this when you have a very simple function on an event handler.
+//
 
 
 export default SearchBar;
